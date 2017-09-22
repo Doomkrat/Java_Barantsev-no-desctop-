09f7d6dfb779afdea6303556a549a04e6acc8886
Sergii <sergeyshovkunenko@gmail.com>
2017-09-21 22:12:35 -0700

    Добавлено несколько ф-ций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -4,6 +4,28 @@ public class myFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+      hello("world");
+      hello("user");
+      hello("sergii");
+
+      double l=5;
+      System.out.println("square sq with one side"+l+" = "+ area(l));
+
+      double a=5;
+      double b=6;
+      System.out.println("reqtang square "+a+" and "+b+" = "+area(a,b));
+
+
+    }
+
+    public static void hello(String somebody){
+      System.out.println("Hello, " + somebody + "!");
+
+    }
+    public static double area (double len){
+      return len * len;
+    }
+    public static double area(double a, double b){
+      return a*b;
     }
 }
