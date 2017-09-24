99457390a52285b965880961fc6f5870e5701669
Sergii <sergeyshovkunenko@gmail.com>
2017-09-23 19:43:13 -0700

    2.2 Создаем ф-цию public static double distance(Point p1, Point p2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -3,8 +3,10 @@ package ru.stqa.pft.sandbox;
 public class Point1 {
   public double x;
   public double y;
-  public Point1(double x,double y ){
-    this.x=x;
-    this.y=y;
+
+  public static double distance1(Point1 p1, Point1 p2){
+    double xdif=p1.x-p2.x;
+    double ydif=p1.y-p2.y;
+    return Math.sqrt((xdif*xdif + ydif*ydif));
   }
 }
