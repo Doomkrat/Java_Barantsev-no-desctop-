b9a901abb312e1983f9de1af027d5ba3c249fa88
Sergii <sergeyshovkunenko@gmail.com>
2017-09-24 06:56:23 -0700

    Решение пункта 2.4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.sandbox;
 public class Point1 {
   public double x;
   public double y;
-
-
+public Point1 (double x, double y){
+  this.x=x;
+  this.y=y;
+  }
+  public double distance1(Point1 p){
+    return Math.sqrt((this.x-p.x)*(this.x-p.x)+(this.y-p.y)*(this.y-p.y));
+  }
 }
