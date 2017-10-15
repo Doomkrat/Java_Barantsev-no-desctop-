9119124511150c913a02405b1b68e1ece66cdbef
Sergii <sergeyshovkunenko@gmail.com>
2017-10-15 15:40:46 -0700

    создан Fluent interface для автотестов Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions(){
     app.goTo().groupPage();
     if (app.group().list().size()==0){
-      app.group().create(new GroupData("Sergii", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
   @Test
@@ -22,7 +22,8 @@ public class GroupModificationTests extends TestBase {
 
     List<GroupData> before = app.group().list();
     int index = before.size()-1;
-    GroupData group = new GroupData(before.get(index).getId(),"Sergii", "ThisHeader", "ThisFooter");
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId()).withName("Sergii").withHeader("ThisHeader").withFooter("ThisFooter");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(),before.size());
