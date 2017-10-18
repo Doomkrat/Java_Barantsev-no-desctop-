1c8a13d90ea4fecdd4b0d723e921f34ff6b9523c
Sergii <sergeyshovkunenko@gmail.com>
2017-10-17 20:51:53 -0700

    Реализованы проверки в fluent-стиле для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
@@ -9,6 +9,7 @@ public class Groups extends ForwardingSet<GroupData> {
   private Set<GroupData> delegate;
 
   public Groups(Groups groups) {
+
     this.delegate = new HashSet<GroupData>(groups.delegate);
   }
 
@@ -18,7 +19,8 @@ public class Groups extends ForwardingSet<GroupData> {
 
   @Override
   protected Set<GroupData> delegate() {
-    return null;
+
+    return delegate;
   }
 
   public Groups withAdded(GroupData group){
@@ -30,5 +32,12 @@ public class Groups extends ForwardingSet<GroupData> {
     Groups groups =new Groups(this);
     groups.remove(group);
     return groups;
+  }
+  public Groups withModified(GroupData modifiedGroup, GroupData group ){
+    Groups groups = new Groups(this);
+    groups.remove(modifiedGroup);
+    groups.add(group);
+    return groups;
+
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,15 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
-
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,8 +13,8 @@ public class GroupDeletionTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0) {
-      app.group().create(new GroupData().withName("test1"));
+    if (app.group().all().size() == 0) {
+      app.group().create(new GroupData().withName("Sergii"));
     }
   }
 
@@ -31,7 +25,6 @@ public class GroupDeletionTests extends TestBase {
     app.group().delete(deletedGroup);
     Groups after = app.group().all();
     assertEquals(after.size(), before.size() - 1);
-
 
     assertThat(after, equalTo(before.without(deletedGroup)));
   }
