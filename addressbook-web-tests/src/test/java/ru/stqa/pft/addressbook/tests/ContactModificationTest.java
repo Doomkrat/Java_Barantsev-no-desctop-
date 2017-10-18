696d86342008c38463a3944a6434500c99a73908
Sergii <sergeyshovkunenko@gmail.com>
2017-10-17 17:54:54 -0700

    Реализованы проверки в fluent-стиле для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -1,11 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactModificationTest extends TestBase {
   @BeforeMethod
@@ -18,18 +24,16 @@ public class ContactModificationTest extends TestBase {
   @Test
   public void contactModificationTests(){
 
-    Set<ContactData> before = app.contact().all();
+    Contacts before = app.contact().all();
     ContactData modifiedContact = before.iterator().next();
     ContactData contact = new ContactData()
             .withId(modifiedContact.getId()).withFirstname("Aram").withLastname("Zamzam")
             .withMobilephone("323123").withEmail("johnymnemomonik@gmail.com");
     app.contact().modify(contact);
-    Set<ContactData> after = app.contact().all();
+    Contacts after = app.contact().all();
     app.goTo().homePage();
 
-    before.remove(modifiedContact);
-    before.add(contact);
-    Assert.assertEquals(before,after);
+    assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
   }
 
 
