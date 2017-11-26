b5c054aed473322e8fe3eada5ecbcfd8aaba03cc
Sergii <sergeyshovkunenko@gmail.com>
2017-11-25 23:23:58 -0800

    Registration Test с отключением капчи (неработает)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
   protected static ApplicationManager app;
@@ -13,10 +16,13 @@ public class TestBase {
   public void setUp() throws Exception {
     app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"),
+            "config_inc.php","config_inc.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.bak","config_inc.php");
     app.stop();
   }
 
