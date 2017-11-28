ac01d717617497eddccefff185e7c9c6d3eb87ba
Sergii <sergeyshovkunenko@gmail.com>
2017-11-27 22:57:38 -0800

    пуш

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -41,7 +41,7 @@ $g_db_type                = 'mysqli';
 $g_database_name          = 'bugtracker';
 $g_db_username            = 'root';
 $g_db_password            = '';
-$g_signup_use_capcha      = OFF;
+$g_signup_use_captcha      = OFF;
 $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'KJ/ln9zgGJBFP0VEhoIdZJ+QRhKlDu847XUlaNJzGGY=';
